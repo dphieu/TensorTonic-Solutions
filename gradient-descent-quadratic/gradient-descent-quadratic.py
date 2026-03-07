f400a74758a4e1242edba336dce456eae8f837f6
dphieu <127303105+dphieu@users.noreply.github.com>
2026-03-07 20:00:08 +0700

    Update solution for gradient-descent-quadratic

--- gradient-descent-quadratic/gradient-descent-quadratic.py
+++ gradient-descent-quadratic/gradient-descent-quadratic.py
@@ -3,9 +3,9 @@ def gradient_descent_quadratic(a, b, c, x0, lr, steps):
     Return final x after 'steps' iterations.
     """
     # Write code here
-    def dera_f(x):
+    def dera(x):
         return 2 * a * x + b
 
     for i in range(steps):
-        x0 = x0 - lr * dera_f(x0)
+        x0 = x0 - lr * dera(x0)
     return x0
